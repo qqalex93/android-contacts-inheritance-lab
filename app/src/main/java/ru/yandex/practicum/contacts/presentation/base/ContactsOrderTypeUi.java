d57beffa64fa91d293c0938c510c75e4b0e734be
Aliaksandr Kasnikouski <kosnikovskii1993@gmail.com>
2024-09-09 16:41:10 +0300

    Отредачил. Заработало на телефоне

---
+++ app/src/main/java/ru/yandex/practicum/contacts/presentation/base/ContactsOrderTypeUi.java
@@ -0,0 +1,22 @@
+package ru.yandex.practicum.contacts.presentation.base;
+
+import androidx.annotation.NonNull;
+
+public abstract class ContactsOrderTypeUi {
+
+    protected String type;
+    protected boolean isSelected;
+
+    public ContactsOrderTypeUi(@NonNull String type, boolean isSelected) {
+        this.type = type;
+        this.isSelected = isSelected;
+    }
+
+    public String getType() {
+        return type;
+    }
+    public boolean isSelected() {
+        return isSelected;
+    }
+    public abstract String createLogMessage();
+}
